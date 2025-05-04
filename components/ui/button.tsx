358b9d873d4b2c512d96c81d12b5194bf3f05ddd
Alekanedero <alexandrecompin@gmail.com>
2025-05-04 19:39:32 +0200

    3 - build and deploy

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -28,6 +28,7 @@ const buttonVariants = cva(
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
         sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
         lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
+        xl: "h-12 rounded-md px-8 has-[>svg]:px-6",
         icon: "size-9",
       },
     },
@@ -37,6 +38,12 @@ const buttonVariants = cva(
     },
   }
 );
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean;
+}
 
 function Button({
   className,
